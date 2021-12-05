3181e4876f23843bfb1e46bc222fccccedce9572
rafavilomar <rafavilomar@gmail.com>
2021-12-05 14:12:39 -0400

    fix: add comment routers to index #8

--- src/Routes/CommentRoutes.ts
+++ src/Routes/CommentRoutes.ts
@@ -4,9 +4,11 @@ import CommentController from "../Controllers/CommentController";
 const router = express.Router();
 let commentController = new CommentController();
 
-router.get("byTeacher/:id", getCommentByTeacher);
+router.get("/byTeacher/:id", getCommentByTeacher);
 
 async function getCommentByTeacher(req: any, res: any) {
-  let response = commentController.getCommentByTeacher();
+  let response = await commentController.getCommentByTeacher();
   res.json(response);
 }
+
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import config from "../config";
+
+// ROUTES
 import TeacherRoutes from "./Routes/TeacherRoutes";
+import CommentRoutes from "./Routes/CommentRoutes";
 
 const app = express();
 
@@ -8,6 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.use("/teacher", TeacherRoutes);
+app.use("/comment", CommentRoutes);
 
 app.listen(config.api.port, () => {
   console.log(`Listening on port: ${config.api.port}`);
